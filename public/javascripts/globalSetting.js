42a8eb67d143ac28de7b88557e44e32c8368baeb
RJ Chang <loch0728@gmail.com>
2022-05-31 04:00:09 +0800

    Adding function of clicking alert window button

--- public/javascripts/globalSetting.js
+++ public/javascripts/globalSetting.js
@@ -11,6 +11,11 @@ function getRandom(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+// 點擊警告視窗確認按鈕
+document.getElementById('alert-window-confirm-btn').addEventListener('click', () => {
+    document.location.reload();
+})
+
 // 產生文字節點
 function generateText(string){
     return document.createTextNode(string);
