30ebb05f59f71a32d1f2e02e74d6a5c4b3370997
RJ Chang <loch0728@gmail.com>
2022-05-31 23:44:17 +0800

    Adding dotenv for using environment variables

--- model.js
+++ model.js
@@ -1,38 +1,24 @@
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://anfer:Anferdb0728@cluster0.nabgq.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// client.connect(err => {
-//     const collection = client.db("17tesyun99").collection("members");
-//     collection.find({account:"test@test"}).toArray(function(err, results){
-//         console.log("結果勒", results); // output all records
-//     });
-
-//     client.close();
-// });
-
-
+require('dotenv').config() // 使用 dotenv 引入環境變數
+// 連線至 MongoDB
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://anfer:Anferdb0728@cluster0.nabgq.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nabgq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function getData(email){
+async function getData(email){ // 取得會員資料 function
     try {
         await client.connect();
         const database = client.db("17tesyun99");
         const results = database.collection("members");
         
         const query = { account: email };
-        
         const result = await results.findOne(query);
-        
-        console.log('getData 的結果', result);
+        // console.log('getData 的結果', result);
 
         return result;
 
-      } finally {
-        await client.close();
-      }
+    } finally {
+      await client.close();
+    }
 }
 
 module.exports = getData;
